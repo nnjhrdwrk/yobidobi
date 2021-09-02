d3bf2a0e402a5ecfefe65c53991b20af2f4192ec
nnjhrdwrk <nnjhrdwrk95@gmail.com>
2021-09-02 12:45:08 +0300

    2

--- js/my-scripts.js
+++ js/my-scripts.js
@@ -56,3 +56,32 @@ $(document).ready(function() {
 	});
 
 });
+
+
+$(document).ready(function (){
+  $('a[href^="#"]').click(function() {
+    var target = $(this).attr('href');
+    $('html, body').animate({
+      scrollTop: $(target).offset().top
+    }, 100);
+  });
+});
+
+
+
+$(document).ready(function(){
+ 
+$(window).scroll(function(){
+if ($(this).scrollTop() > 100) {
+$('.scrollup').fadeIn();
+} else {
+$('.scrollup').fadeOut();
+}
+});
+ 
+$('.scrollup').click(function(){
+$("html, body").animate({ scrollTop: 0 }, 600);
+return false;
+});
+ 
+});
